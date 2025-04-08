494ed27b32bd2b107ee1afa773340b3d14e3a760
Kuro <khoic3b1999@gmail.com>
2025-04-08 19:54:42 +0700

    [INIT] init base

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SharedModule } from './shared/shared.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideNoopAnimations(),
+    provideAnimationsAsync(),
+    SharedModule
+  ]
 };
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    //Module
+    CommonModule,
+
+  ],
+  exports: [
+    CommonModule,
+
+  ],
+  providers: [],
+})
+export class SharedModule { }
